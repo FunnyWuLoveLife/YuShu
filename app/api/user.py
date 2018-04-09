02c2bbb129a41616ea69f6f7cde03c8941e46777
Funnywu <agiot1026@163.com>
2018-04-09 22:04:49 +0800

    add api user

---
+++ app/api/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+# @file: user.py
+# @time: 2018/4/9 17:15
+# @author: FunnyWu
+# @contact: agiot1026@163.com
+# @Software: PyCharm
+
+
+def user_info():
+    pass
