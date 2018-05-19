a73a25dfa2fdf9bc22194abd400fd27ec87f9778
Funnywu <agiot1026@163.com>
2018-05-19 18:32:37 +0800

    updated spider

---
+++ spider/qudaxueSpider.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+# @file: qudaxueSpider.py
+# @time: 2018/5/19 17:47
+# @author: FunnyWu
+# @contact: agiot1026@163.com
+# @Software: PyCharm
+
+access_token = 'c19d26d2-6149-4c8f-b499-20062b00fb61'
+"""
+nickName=吴银&
+gender=1&
+language=zh_CN&
+city=Kaixian&
+province=Chongqing&
+country=China&
+avatarUrl=https://wx.qlogo.cn/mmopen/vi_32/3TD9tAMYusKvbR7riaHlOGdteQLSoHAdeicR32P9gic3jUxnnvYZ0C4gBFkIL14nHTEzc6NB9VMrSPZNFNu2IutYw/132&code=033QPiHi0ReHCm1sOdIi09zjHi0QPiHL
+"""
